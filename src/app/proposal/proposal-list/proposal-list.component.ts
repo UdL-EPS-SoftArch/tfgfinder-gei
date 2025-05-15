621459599ca28a11387f6f21127945a20f274428
Aleix-CB <aleix.cerqueda@gmail.com>
2025-05-15 16:31:33 +0200

    ProposalList_feature

--- src/app/proposal/proposal-list/proposal-list.component.ts
+++ src/app/proposal/proposal-list/proposal-list.component.ts
@@ -1,36 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { ProposalService } from '../proposal.service';  // Adjust path as needed
-import { Proposal } from '../proposal';              // Adjust path as needed
+import { Proposal } from '../proposal';
+import { Router, RouterLink } from '@angular/router';
+import {PagedResourceCollection} from "@lagoshny/ngx-hateoas-client";
+import {NgbPagination} from "@ng-bootstrap/ng-bootstrap";
+import {UserSearchComponent} from "../../user/user-search/user-search.component";
+import {NgForOf} from "@angular/common";            // Adjust path as needed
 
 @Component({
+  imports: [RouterLink, NgbPagination, NgForOf],
   selector: 'app-proposal-list',
   templateUrl: './proposal-list.component.html'
 })
 export class ProposalListComponent implements OnInit {
   proposals: Proposal[] = [];
   page = 1;
-  pageSize = 10;
+  pageSize = 5;
   totalProposals = 0;
 
-  constructor(private proposalService: ProposalService) {}
+  constructor(
+    public router: Router,
+    private proposalService: ProposalService) {
 
-  ngOnInit(): void {
-    this.fetchProposals();
   }
 
-  fetchProposals(): void {
-    this.proposalService.getPage(this.page - 1, this.pageSize).subscribe(response => {
-      this.proposals = response.content; // assuming Spring-style paginated response
-      this.totalProposals = response.totalElements;
-    });
+  ngOnInit(): void {
+    this.proposalService.getPage({ pageParams:  { size: this.pageSize }, sort: { proposal: 'ASC' } }).subscribe(
+      (page: PagedResourceCollection<Proposal>) => {
+        this.proposals = page.resources;
+        this.totalProposals = page.totalElements;
+      });
   }
 
   changePage(): void {
-    this.fetchProposals();
+    this.proposalService.getPage({ pageParams: { page: this.page - 1, size: this.pageSize }, sort: { proposal: 'ASC' } }).subscribe(
+      (page: PagedResourceCollection<Proposal>) => this.proposals = page.resources);
   }
 
-  detail(event: any): void {
-    this.proposals = event.results;  // assuming event carries filtered proposals
-    this.totalProposals = event.total || event.results.length;
+  detail(proposal: Proposal): void {
+    this.router.navigate(['proposals', proposal.id]);
   }
 }
