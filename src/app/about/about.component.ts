472806e0266810476d2d09582a0f0cf179e64435
Roberto García <roberto@rhizomik.net>
2025-05-29 19:59:00 +0200

    Fix internal link to use Angular syntax

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,9 +1,13 @@
 import {Component} from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
-    selector: 'app-about',
-    templateUrl: './about.component.html',
-    styleUrls: ['./about.component.css']
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  imports: [
+    RouterLink
+  ],
+  styleUrls: ['./about.component.css']
 })
 export class AboutComponent {
 
