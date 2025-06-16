9b29b15ffff199e52185a95209e0b7632f446695
Gabriel <gb9@alumnes.udl.cat>
2025-06-16 22:46:41 +0200

    @HateoasResource('categories') added

--- src/app/category/category.ts
+++ src/app/category/category.ts
@@ -1,5 +1,6 @@
-import { Resource } from '@lagoshny/ngx-hateoas-client';
+import {HateoasResource, Resource} from '@lagoshny/ngx-hateoas-client';
 
+@HateoasResource('categories')
 export class Category extends Resource {
   id?: number;
   name: string = '';
